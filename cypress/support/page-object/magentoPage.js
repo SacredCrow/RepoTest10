4978ace791005bb5fbf0d389a751883706a4c400
SacredCrow <anggadit.p@gmail.com>
2025-02-22 23:48:40 +0700

    Update POM

---
+++ cypress/support/page-object/magentoPage.js
@@ -0,0 +1,24 @@
+class magentoPage {
+  signin_label = ".panel > .header > .authorization-link > a";
+  email_textbox = "#email";
+  pass_textbox = ".login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass";
+  signin_button = ".login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span";
+
+  clickSigninLabel() {
+    cy.get(this.signin_label).click();
+  }
+
+  inputEmailTextbox(n) {
+    cy.get(this.email_textbox).type(n);
+  }
+
+  inputPassTextbox(n) {
+    cy.get(this.pass_textbox).type(n);
+  }
+
+  clickSigninButton() {
+    cy.get(this.signin_button).click();
+  }
+}
+
+export default new magentoPage();
