2b2fc32449cb629ee5f8b0eccac8c49c86066769
SacredCrow <anggadit.p@gmail.com>
2025-02-23 20:35:25 +0700

    Update Login

--- cypress/e2e/magento/login.cy.js
+++ cypress/e2e/magento/login.cy.js
@@ -7,7 +7,7 @@ describe("Verify Login Functionality", () => {
 
   it("Success Login", () => {
     magentoPage.clickSigninLabel();
-    magentoPage.inputEmailTextbox("budiyono@MediaList.com");
+    magentoPage.inputEmailTextbox("budiyono@mail.com");
     magentoPage.inputPassTextbox("Coba123#");
     magentoPage.clickSigninButton();
     cy.get(".logo > img").should("be.visible");
@@ -16,21 +16,21 @@ describe("Verify Login Functionality", () => {
 
   it("Failed Login - Wrong Email", () => {
     magentoPage.clickSigninLabel();
-    magentoPage.inputEmailTextbox("budiyon0@MediaList.com");
+    magentoPage.inputEmailTextbox("budiyon0@mail.com");
     magentoPage.inputPassTextbox("Coba123#");
     magentoPage.clickSigninButton();
-    cy.get(".message-error-pan").should("be.visible");
+    cy.get(".message-error").should("be.visible");
     cy.get(".message-error").should("contain.text", "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.");
     cy.url().should("include", "login");
   });
 
   it("Failed Login - Wrong Password", () => {
     magentoPage.clickSigninLabel();
-    magentoPage.inputEmailTextbox("budiyono@MediaList.com");
+    magentoPage.inputEmailTextbox("budiyono@mail.com");
     magentoPage.inputPassTextbox("Salahsalah");
     magentoPage.clickSigninButton();
     cy.get(".message-error").should("be.visible");
-    cy.get(".message-error").should("have.text", "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.");
+    cy.get(".message-error").should("contain.text", "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.");
     cy.url().should("include", "login");
   });
 });
